72ec62aa61e2182274a31468690c7f159d2e83c3
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-06-22 15:13:10 +0000

    Final commit

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -64,7 +64,7 @@ def delete_task(task_id):
     task = Task.query.get_or_404(task_id)
     db.session.delete(task)
     db.session.commit()
-    return redirect(url_for("add_task"))  
+    return redirect(url_for("home"))  
 
 @app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
