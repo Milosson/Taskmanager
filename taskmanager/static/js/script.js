92be5b0f548623d3beed484bbb086462923d76b6
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-06-22 14:24:02 +0000

    Implemented edit_task.html, added relevent routing to routes.py, added button design to style.css

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -14,3 +14,22 @@ document.addEventListener('DOMContentLoaded', function() {
   // select initialization
   let selects = document.querySelectorAll('select');
   M.FormSelect.init(selects);
+
+// collapsible initialization
+let collapsibles = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsibles);
+
+
+// Delete confirmation function
+function confirmDelete(actionType) {
+  var message = "";
+  if (actionType === "task") {
+      message = "This action will delete this task, are you sure?";
+  } else if (actionType === "category") {
+      message = "This action will delete this category, are you sure?";
+  } else {
+      message = "Are you sure you want to proceed?";
+  }
+
+  return confirm(message);
+}
