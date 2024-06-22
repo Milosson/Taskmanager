c42d693efd14f506a33ef09cb6a231358bb2272b
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-06-22 16:15:05 +0000

    L1

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,24 +1,25 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-if os.path.exists("env.py"):
-    import env # noqa
-
-
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-if os.environ.get("DEVELOPMENT") == True:
+# Check if in development mode
+if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Add this line
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
+    if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
-    
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
+# Initialize SQLAlchemy
 db = SQLAlchemy(app)
 
-from taskmanager import routes  # noqa
+# Import routes after initializing db to avoid circular imports
+from taskmanager import routes
+
+if __name__ == "__main__":
+    app.run()
